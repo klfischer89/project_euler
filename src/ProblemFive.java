59f10191e4542d1d79433f88b2b64411e53f354b
klfischer89 <fischer-k4@web.de>
2025-12-04 16:20:28 +0100

    starting with problem ID 5

---
+++ src/ProblemFive.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProblemFive {
+
+    public static void solve(){
+
+        List<Integer> div = new ArrayList<>();
+
+        for (int i = 1; i <= 20; i++) {
+            div.add(i);
+        }
+    }
+
+}
